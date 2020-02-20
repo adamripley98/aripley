72ab116f1e61018fe7de0bba11e031b40c4eb6c8
Adam Ripley <aripley@wharton.upenn.edu>
2020-02-20 08:03:55 -0500

    home page work

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,38 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import FB from '../images/fb.png'
+import Linkedin from '../images/linkedin.png'
+import Git from '../images/git.png'
+import Insta from '../images/ig.png'
+import Adam from '../images/prof.jpg'
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="row">
+        <div className="col-lg-4">
+          <img src={Adam} alt="adam" className="headshot"/>
+        </div>
+        <div className="col-lg-8">
+          <h1>Hi, I'm <span className="green-text">Adam.</span></h1>
+          <p className="p-large">
+            I’m a student, <span><u>web developer,</u></span> and product builder from Anchorage, Alaska.
+            I currently attend the <span><u>University of Pennsylvania</u></span> where I study entrepreneurial management, computer science, and psychology.
+            I’m passionate about <span><u>A LOT,</u></span> but at the end of the day I’m driven mostly by the idea of building cool products with cool people.
+            Check out what I’m <span><u>working on,</u></span> get weirded out by my <span><u>thoughts,</u></span> and <span><u>say hi.</u></span>
+          </p>
+          <div className="social-group">
+            <img src={FB} alt="fb" className="social-icon" />
+            <img src={Linkedin} alt="linkedin" className="social-icon" />
+            <img src={Git} alt="git" className="social-icon" />
+            <img src={Insta} alt="insta" className="social-icon" />
+          </div>
+        </div>
     </div>
+    <div className="clear"/>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
