ec18c5abc59c3c3b9f69622cb10bf4bfb1800b26
Adam Ripley <aripley@wharton.upenn.edu>
2020-02-21 20:15:32 -0500

    what up

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,25 +7,14 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle="Adam Ripley" />
       <div className="container animate fadeIn">
         <main>{children}</main>
       </div>
